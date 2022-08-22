c5941ebc6dc52ebae937a86d535b60cdf7084db7
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-22 16:58:12 -0300

    feat(Cards): styled card finished

--- components/Card/card.vue
+++ components/Card/card.vue
@@ -5,7 +5,7 @@
       w="200px"
       boxShadow="xl"
       bg="white"
-      min-h="400px"
+      min-h="430px"
       display="flex"
       flex-dir="column"
       justify-content="space-between"
@@ -13,16 +13,37 @@
       pb="{2}"
       rounded="lg"
       cursor="pointer"
+      :_hover="{ transform: 'translateY(-10px)', borderColor: 'gray.100'}"
     >
       <CImage
         :src="movie.poster_path"
         :alt="movie.title"
-        min-h="300px"
+        h="275px"
         rounded="lg"
       />
-      <CStack>
-        <CText font-weight="bold" text-align="center"> {{ movie.title }}</CText>
-        <CButton left-icon="cart-shopping">
+      <c-icon-button
+        aria-label="Favoritos"
+        variant="ghost"
+        icon="heart"
+        color="white"
+        font-size="1.5rem"
+        top="5px"
+        right="5px"
+        position="absolute"
+        :_hover="{color: 'red.600' }"
+      />
+      <CStack mb="0">
+        <CText font-weight="bold" font-size="1.1rem" text-align="center">
+          {{ movie.title }}
+        </CText>
+        <CFlex flex-dir="row" align-items="center" justify-content="center" gap="2">
+          <c-icon name="star" size="1rem" />
+          <c-heading size="sm" pl='2'>{{ movie.vote_average }} </c-heading>
+        </CFlex>
+        <CText text-align="center">
+          R$ 19,90
+        </CText>
+        <CButton left-icon="cart-shopping" bg="indigo.400" color="white" :_hover="{bg: 'indigo.600' }">
           Adicionar
         </CButton>
       </CStack>
@@ -32,14 +53,16 @@
 
 <script>
 // eslint-disable-next-line quotes
-import { CBox, CImage, CStack, CButton } from "@chakra-ui/vue"
+import { CBox, CImage, CStack, CButton, CHeading, CIconButton } from "@chakra-ui/vue"
 export default {
   name: 'CardItem',
   components: {
     CBox,
     CImage,
     CStack,
-    CButton
+    CButton,
+    CHeading,
+    CIconButton
   },
   props: { movie: Object }
 }
