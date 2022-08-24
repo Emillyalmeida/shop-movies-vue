1c19ef9ded2a5b92fd9c739f8c4a188538c69b38
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-24 18:42:59 -0300

    feat(header): add box with length of the cart

--- components/Header/header.vue
+++ components/Header/header.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div width="100%">
     <Cart :isOpen="isOpen" :close="close" />
     <Favorites :isOpen="isOpenFav" :close="closeFav" />
     <CFlex
       align="center"
-      w="100vw"
+      w="calc(100vw - 15px)"
       bg="vue.400"
       :h="['11vh', '11vh', '12vh']"
       color="white"
@@ -12,7 +12,7 @@
       padding-x="4"
       justify-content="space-between"
     >
-      <c-heading as="h1" size="xl">Movie Shopp</c-heading>
+      <c-heading as="h1" size="xl" pl="4">Movie Shop</c-heading>
       <CFlex padding-x="6">
         <c-icon-button
           aria-label="Favoritos"
@@ -33,8 +33,25 @@
           font-size="1.8rem"
           size="lg"
           :_hover="{borderWidth: '1px', borderColor: 'white' }"
+          pos="relative"
           @click="isOpen= true"
         />
+        <CBox
+          rounded="full"
+          bg="yellow.400"
+          w="25px"
+          h="25px"
+          display="flex"
+          justify-content="center"
+          align-items="center"
+          pos="absolute"
+          right="32px"
+          top="16px"
+        >
+          <c-heading size="md">
+            {{ getCart.length }}
+          </c-heading>
+        </CBox>
       </CFlex>
     </CFlex>
   </div>
@@ -45,12 +62,13 @@
 import {
   CFlex,
   CHeading,
-  CIconButton
+  CIconButton,
+  CBox
 
 } from '@chakra-ui/vue'
+import { mapGetters } from 'vuex'
 import Cart from '../Cart/Cart.vue'
 import Favorites from '../Favorites/Favorites.vue'
-
 export default {
   name: 'HeaderComp',
   components: {
@@ -58,13 +76,20 @@ export default {
     CFlex,
     CHeading,
     Cart,
-    Favorites
+    Favorites,
+    CBox
   },
   data () {
     return {
       isOpen: false,
       isOpenFav: false
     }
+  },
+  computed: {
+    ...mapGetters([
+      'getCart',
+      'sumTotal'
+    ])
   },
   methods: {
     close () {
