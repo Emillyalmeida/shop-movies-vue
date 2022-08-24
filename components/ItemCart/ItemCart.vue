79f9c474dd2aa2d81d988d6ffa05031f1f95656b
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-24 14:33:02 -0300

    feat(Cart): implement mutation in the buttons and adjust style

--- components/ItemCart/ItemCart.vue
+++ components/ItemCart/ItemCart.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <CFlex w="100%" p="2" justify-content="space-between" align-items="center">
-      <CImage :src="movie.poster_path" :alt="movie.title" boxSize="100px" objectFit="contain"/>
+    <CFlex w="100%" p="2" px="['0','2']" justify-content="space-between" align-items="center">
+      <CImage :src="movie.poster_path" :alt="movie.title" boxSize="100px" objectFit="contain" />
       <c-stack align-items="center">
         <c-heading text-align="center" font-size="lg">
           {{ movie.title }}
@@ -10,14 +10,20 @@
       </c-stack>
       <c-stack spacing="2" pr="2">
         <CFlex align-items="center">
-          <c-icon-button variant="ghost" size="sm" aria-label="adicionar" icon="add" />
+          <c-icon-button variant="ghost" size="sm" aria-label="adicionar" icon="add" @click="$store.commit('addQtd', movie)" />
           <CText px="2">
-            {{ movie.qtd }}
+            {{ $store.getters.getMovie(movie).qtd }}
           </CText>
-          <c-icon-button variant="ghost" size="sm" aria-label="diminuir" icon="minus" />
+          <c-icon-button variant="ghost" size="sm" aria-label="diminuir" icon="minus" @click="$store.commit('removeQtd', movie)" />
         </CFlex>
       </c-stack>
-      <c-icon-button variant="ghost"  size="lg" aria-label="remover" icon="trash-can" />
+      <c-icon-button
+        variant="ghost"
+        font-size="1.5rem"
+        aria-label="remover"
+        icon="trash-can"
+        @click="$store.commit('removeMovie', movie)"
+      />
     </CFlex>
   </div>
 </template>
