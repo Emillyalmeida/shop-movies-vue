961d8ac7b37945ff6d4120de532cdcc122406e11
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-25 11:17:33 -0300

    feat(checkout): add modal to success

---
+++ components/ModalSuccess/ModalSuccess.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <c-modal
+      :is-open="isOpen"
+      :on-close="close"
+      is-centered
+    >
+      <c-modal-content ref="content" border-width="2px" border-color="gray.200">
+        <c-modal-header> Obridado(a) {{ user }}!</c-modal-header>
+        <c-modal-close-button />
+        <c-modal-body>
+          <c-text>
+            Sua Compra foi finalizada com sucesso!
+          </c-text>
+        </c-modal-body>
+        <c-modal-footer>
+          <c-button
+            bg="indigo.400"
+            color="white"
+            :_hover="{bg: 'indigo.600' }"
+            h="50px"
+            w="100%"
+            font-size="1.25rem"
+            @click="go"
+          >
+            Ir para a Loja
+          </c-button>
+        </c-modal-footer>
+      </c-modal-content>
+      <c-modal-overlay />
+    </c-modal>
+  </div>
+</template>
+
+<script>
+import {
+  CModal,
+  CModalOverlay,
+  CModalContent,
+  CModalHeader,
+  CModalFooter,
+  CModalBody,
+  CModalCloseButton,
+  CText
+} from '@chakra-ui/vue'
+export default {
+  name: 'ModalSuccess',
+  components: {
+    CModal,
+    CModalOverlay,
+    CModalContent,
+    CModalHeader,
+    CModalFooter,
+    CModalBody,
+    CModalCloseButton,
+    CText
+  },
+  props: { isOpen: Boolean, close: Function, user: String },
+  methods: {
+    go () {
+      return this.$router.push('/')
+    }
+  }
+}
+</script>
