d6fc1ba44382f75ea5a0a30cd0d6cd838243aa98
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-26 09:40:06 -0300

    feat(indeX): create component loading

---
+++ components/loading/loading.vue
@@ -0,0 +1,25 @@
+<template>
+  <CGrid
+    w="100%"
+    template-columns="repeat(auto-fill, minmax(200px,220px))"
+    justify-content="center"
+    gap="10"
+    p="8"
+  >
+    <c-spinner v-for="numbers in 10" :key="numbers" w="200px" h="200px" />
+  </CGrid>
+</template>
+
+<script>
+import {
+  CGrid,
+  CSpinner
+} from '@chakra-ui/vue'
+export default {
+  name: 'LoadingComp',
+  components: {
+    CGrid,
+    CSpinner
+  }
+}
+</script>
